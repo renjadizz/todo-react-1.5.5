278bd3855e7887601a77c2518e63d04b7b7fb014
Murat <renja.dizz@gmail.com>
2023-11-21 18:40:35 +0600

    change Task to functional component

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,29 +1,21 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { formatDistanceToNow } from 'date-fns'
 
 import Timer from '../Timer/Timer'
 import './Task.css'
 
-class Task extends React.Component {
-  render() {
-    const { itemText } = this.props
-    const formatedDate = formatDistanceToNow(new Date(itemText.created), { includeSeconds: true })
-    const dateToShow = `created ${formatedDate} ago`
-    return (
-      <>
-        <span className="title">{itemText.description}</span>
-        <span className="description">
-          <Timer timer={itemText.timer} />
-        </span>
-        <span className="created">{dateToShow}</span>
-      </>
-    )
-  }
-}
-
-Task.propTypes = {
-  itemText: PropTypes.object.isRequired,
+function Task({ itemText }) {
+  const formatedDate = formatDistanceToNow(new Date(itemText.created), { includeSeconds: true })
+  const dateToShow = `created ${formatedDate} ago`
+  return (
+    <>
+      <span className="title">{itemText.description}</span>
+      <span className="description">
+        <Timer timer={itemText.timer} />
+      </span>
+      <span className="created">{dateToShow}</span>
+    </>
+  )
 }
 
 export default Task
