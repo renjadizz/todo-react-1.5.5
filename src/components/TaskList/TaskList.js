8d578ef4c4d1b0ba362e0717fd0c6730e57f2b46
Murat <renja.dizz@gmail.com>
2023-11-21 19:58:59 +0600

    use hooks in TaskList

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,64 +1,49 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react'
 
 import Task from '../Task/Task'
 import './TaskList.css'
 
-export default class TaskList extends React.Component {
-  state = {
-    value: '',
+function TaskList({ taskItems, onChangeTaskState, onDeleteTaskItem, onEditStateTaskItem, onEditTaskItem }) {
+  const [value, setValue] = useState('')
+  const onEditClick = (text, id) => {
+    setValue(text)
+    onEditStateTaskItem(id)
   }
-  onEditClick = (text, id) => {
-    this.setState({ value: text })
-    this.props.onEditStateTaskItem(id)
+  const onChange = (e) => {
+    setValue(e.target.value)
   }
-  onChange = (e) => {
-    this.setState({
-      value: e.target.value,
-    })
-  }
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault()
-    if (this.state.value !== '') {
-      this.props.onEditTaskItem(e.target.getAttribute('taskid'), this.state.value)
+    if (value !== '') {
+      onEditTaskItem(e.target.getAttribute('taskid'), value)
     }
   }
-  render() {
-    const { taskItems, onChangeTaskState, onDeleteTaskItem } = this.props
-    const taskItem = taskItems.map((item) => {
-      const { id, state, ...itemText } = item
-      return (
-        <li key={id} className={state}>
-          <div className="view">
-            <input
-              className="toggle"
-              type="checkbox"
-              checked={state === 'completed' ? true : false}
-              onChange={() => onChangeTaskState(id)}
-            />
-            <label>
-              <Task itemText={itemText} />
-            </label>
-            <button className="icon icon-edit" onClick={() => this.onEditClick(itemText.description, id)}></button>
-            <button className="icon icon-destroy" onClick={() => onDeleteTaskItem(id)}></button>
-          </div>
-          {state === 'editing' ? (
-            <form onSubmit={this.onSubmit} taskid={id}>
-              <input type="text" className="edit" value={this.state.value} onChange={this.onChange} />
-            </form>
-          ) : null}
-        </li>
-      )
-    })
-    return <ul className="todo-list">{taskItem}</ul>
-  }
+
+  const taskItem = taskItems.map((item) => {
+    const { id, state, ...itemText } = item
+    return (
+      <li key={id} className={state}>
+        <div className="view">
+          <input
+            className="toggle"
+            type="checkbox"
+            checked={state === 'completed' ? true : false}
+            onChange={() => onChangeTaskState(id)}
+          />
+          <label>
+            <Task itemText={itemText} />
+          </label>
+          <button className="icon icon-edit" onClick={() => onEditClick(itemText.description, id)}></button>
+          <button className="icon icon-destroy" onClick={() => onDeleteTaskItem(id)}></button>
+        </div>
+        {state === 'editing' ? (
+          <form onSubmit={onSubmit} taskid={id}>
+            <input type="text" className="edit" value={value} onChange={onChange} />
+          </form>
+        ) : null}
+      </li>
+    )
+  })
+  return <ul className="todo-list">{taskItem}</ul>
 }
-TaskList.defaultProps = {
-  onChangeTaskState: () => {},
-  onDeleteTaskItem: () => {},
-}
-TaskList.propTypes = {
-  taskItems: PropTypes.arrayOf(PropTypes.object),
-  onChangeTaskState: PropTypes.func,
-  onDeleteTaskItem: PropTypes.func,
-}
+export default TaskList
