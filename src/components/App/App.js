44e4dd24f3c07bada2bb32dc7832acf2963fb763
Murat <renja.dizz@gmail.com>
2023-11-21 20:09:02 +0600

    change App to hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,81 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from '../Header/Header'
 import TaskList from '../TaskList/TaskList'
 import Footer from '../Footer/Footer'
 import './App.css'
 
-export default class App extends React.Component {
-  state = {
-    taskItems: [
-      { id: 1, state: 'completed', description: 'Completed task', created: 1519211809934, timer: 65 },
-      { id: 2, state: 'active', description: 'Editing task', created: 1519211810362, timer: 44 },
-      { id: 3, state: 'active', description: 'Active task', created: 1519211811670, timer: 99 },
-    ],
-    filter: 'all',
-  }
-  onCreateTask = (taskValue, taskTimer) => {
-    const id = this.state.taskItems.length + 1
+function App() {
+  const [taskItems, setTaskItems] = useState([
+    { id: 1, state: 'completed', description: 'Completed task', created: 1519211809934, timer: 65 },
+    { id: 2, state: 'active', description: 'Editing task', created: 1519211810362, timer: 44 },
+    { id: 3, state: 'active', description: 'Active task', created: 1519211811670, timer: 99 },
+  ])
+  const [filter, setFilter] = useState('all')
+  const onCreateTask = (taskValue, taskTimer) => {
+    const id = taskItems.length + 1
     const state = 'active'
     const description = taskValue
     const timer = taskTimer
     const created = Date.now()
-    this.setState(({ taskItems }) => ({
-      taskItems: [...taskItems, { id, state, description, created, timer }],
-    }))
+    setTaskItems([...taskItems, { id, state, description, created, timer }])
   }
-  changeTaskContent = (id, newState) => {
-    this.setState(({ taskItems }) => {
-      const taskItemsLeft = taskItems.map((item) => {
-        if (item.id === id) {
-          return newState(item)
-        }
-        return item
-      })
-      return { taskItems: taskItemsLeft }
+  const changeTaskContent = (id, newState) => {
+    const taskItemsLeft = taskItems.map((item) => {
+      if (item.id === id) {
+        return newState(item)
+      }
+      return item
     })
+    setTaskItems(taskItemsLeft)
   }
-  onEditTaskItem = (id, text) => {
-    this.changeTaskContent(Number(id), (item) => {
+  const onEditTaskItem = (id, text) => {
+    changeTaskContent(Number(id), (item) => {
       const itemDesc = text
       const itemState = 'active'
       return { ...item, state: itemState, description: itemDesc }
     })
   }
-  onEditStateTaskItem = (id) => {
-    this.changeTaskContent(id, (item) => {
+  const onEditStateTaskItem = (id) => {
+    changeTaskContent(id, (item) => {
       const itemState = 'editing'
       return { ...item, state: itemState }
     })
   }
-  onChangeTaskState = (id) => {
-    this.changeTaskContent(id, (item) => {
+  const onChangeTaskState = (id) => {
+    changeTaskContent(id, (item) => {
       const itemState = item.state === 'completed' ? (item.state = 'active') : (item.state = 'completed')
       return { ...item, state: itemState }
     })
   }
-  onDeleteTaskItem = (id) => {
-    this.setState(({ taskItems }) => {
-      const taskItemsLeft = taskItems.filter((item) => item.id !== id)
-      return { taskItems: taskItemsLeft }
-    })
+  const onDeleteTaskItem = (id) => {
+    const taskItemsLeft = taskItems.filter((item) => item.id !== id)
+    setTaskItems(taskItemsLeft)
   }
-  onDeleteCompleted = () => {
-    const completed = this.filterTasks(this.state.taskItems, 'completed')
+  const onDeleteCompleted = () => {
+    const completed = filterTasks(taskItems, 'completed')
     completed.forEach((el) => {
-      this.onDeleteTaskItem(el.id)
+      onDeleteTaskItem(el.id)
     })
   }
-  onFilterChange = (filter) => {
-    this.setState({ filter })
+  const onFilterChange = (filter) => {
+    setFilter(filter)
   }
-  filterTasks = (taskItems, filter) => {
+  const filterTasks = (taskItems, filter) => {
     if (filter !== 'all') {
       return taskItems.filter((el) => el.state === filter)
     }
     return taskItems
   }
-  countActive = (taskItems) => {
+  const countActive = (taskItems) => {
     let counter = 0
     taskItems.forEach((el) => {
       if (el.state === 'active') counter++
@@ -83,28 +75,27 @@ export default class App extends React.Component {
     return counter
   }
 
-  render() {
-    const taskItems = this.filterTasks(this.state.taskItems, this.state.filter)
-    const activeTasks = this.countActive(this.state.taskItems)
-    return (
-      <section className="todoapp">
-        <Header onCreateTask={this.onCreateTask} />
-        <section className="main">
-          <TaskList
-            taskItems={taskItems}
-            onChangeTaskState={this.onChangeTaskState}
-            onDeleteTaskItem={this.onDeleteTaskItem}
-            onEditStateTaskItem={this.onEditStateTaskItem}
-            onEditTaskItem={this.onEditTaskItem}
-          />
-          <Footer
-            filter={this.state.filter}
-            onFilterChange={this.onFilterChange}
-            onDeleteCompleted={this.onDeleteCompleted}
-            activeTasks={activeTasks}
-          />
-        </section>
+  const taskItemsFiltered = filterTasks(taskItems, filter)
+  const activeTasks = countActive(taskItems)
+  return (
+    <section className="todoapp">
+      <Header onCreateTask={onCreateTask} />
+      <section className="main">
+        <TaskList
+          taskItems={taskItemsFiltered}
+          onChangeTaskState={onChangeTaskState}
+          onDeleteTaskItem={onDeleteTaskItem}
+          onEditStateTaskItem={onEditStateTaskItem}
+          onEditTaskItem={onEditTaskItem}
+        />
+        <Footer
+          filter={filter}
+          onFilterChange={onFilterChange}
+          onDeleteCompleted={onDeleteCompleted}
+          activeTasks={activeTasks}
+        />
       </section>
-    )
-  }
+    </section>
+  )
 }
+export default App
